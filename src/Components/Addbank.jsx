f84c5136d53da33b4b6b35404a0be8fba5ad7825
anamsiddiqui-07 <asra.anam94@gmail.com>
2022-07-29 18:13:58 +0530

    29thJuly_main_project

---
+++ src/Components/Addbank.jsx
@@ -0,0 +1,40 @@
+import React, {useState,useEffect} from "react";
+import {TextField, Grid, Card, CardContent, Button,Alert} from "@mui/material"
+export const Addbank=()=>{
+
+
+
+return(
+
+    <div>
+        <Card>
+            <CardContent>
+                <Grid container>
+                    <Grid item xs={4}>
+                        Bank Name:
+                    </Grid>
+                    <Grid item xs={8}>
+                        <TextField label="Enter Bank Name" variant="outlined"/>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                      City Name:
+                    </Grid>
+                    <Grid item xs={8}>
+                        <TextField label="Enter City" variant="outlined"/>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                      Branch Name:
+                    </Grid>
+                    <Grid item xs={8}>
+                        <TextField label="Enter Branch Name" variant="outlined"/>
+                    </Grid>
+
+                </Grid>
+            </CardContent>
+        </Card>
+
+    </div>
+)
+}
