5d18f6d691f95b95b8cc663d6be873ade1fd3cca
Ahad <ahadsheikh@gmail.com>
2022-07-29 19:30:32 +0530

    29July

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 import { Addbank } from './Components/Addbank';
+import './common.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter , Routes, Route} from "react-router-dom"
+import Home from "./Components/Pages/Home";
+import OnlineBanking from "./Components/Pages/OnlineBanking";
+import SearchBank from "./Components/Pages/SearchBank";
+import ContactUs from "./Components/Pages/ContactUs"
 
 function App() {
   return (
-    <div className="App">
-      <Addbank/>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/onlinebanking' element={<OnlineBanking/>}/>
+      <Route path='/searchbank' element={<SearchBank/>}/>
+      <Route path='/contactus' element={<ContactUs/>}/>
+      {/* <Route path='/onlinebanking/debit' element={<Debit/>}/> */}
+    </Routes>
+    </BrowserRouter>
     
-    </div>
   );
 }
 
 export default App;
+
+
+    // <div className="App">
+    //   <Addbank/>
+    
+    // </div>
+ 
---
+++ src/Components/Pages/SearchBank.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchBank() {
+  return (
+    <div>
+        <h1>This is Search Bank</h1>
+    </div>
+  )
+}
+
+export default SearchBank
